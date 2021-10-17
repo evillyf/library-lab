d4a9d03ec7988900b0e4573380b10593200bf0c8
Evilly <61359076+evillyf@users.noreply.github.com>
2021-10-17 19:36:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-# AQUI
-# fazer agora: adicionar imagens no execute https://stackoverflow.com/questions/7674790/bundling-data-files-with-pyinstaller-onefile
-# de acordo com o feedback gerar outras seções
-# gerar outros livros no botao aleatório
+#OBS: No Pycharm é necessário baixar os módulos PILLOW e PILLOW PIL. No terminal instalar o pip install pyinstaller
+#OBS.2: Renomear o arquivo main para a extensão .pyw para excluir a janela preta. No terminal criar executável a partir do código: pyinstaller --onefile main.pyw
+#Depois de criado, retirar da pasta dist e jogar para a pasta do projeto > ctrl+c, ctrl+v para onde você quiser.
+#OBS.3: Necessário ter a versão do webbrowser do navegador(baixar a partir do google), vide sua versão em 'ajuda' > 'sobre o google chrome' - ps: não é preciso estar na mesma pasta.
+#OBS.4: O usuário não precisa baixar nada, só extrair as imagens do arquivo zip do programa.
+
 import tkinter as tk
 import tkinter.font
 import webbrowser
 from tkinter import ttk
 from tkinter.messagebox import showinfo
-
 from PIL import Image, ImageTk
 
 
 
-root = tk.Tk()
-root.resizable(False, False)
-root.geometry("557x692+1+1")
-root.title("LIBRARY-LAB")
-image = Image.open("3.png")
-photo = ImageTk.PhotoImage(image)
-label = tk.Label(root, image=photo, bd=0)
-label.image = photo
-label.grid(row=2, column=0)  # 2
+root = tk.Tk() #criação da janela
+root.resizable(False, False) #não permite a janela ser redimensionado
+root.geometry("557x692+1+1") #tamanho x e y + local fixo que a janela será aberta 
+root.title("LIBRARY-LAB") #título da janela
+image = Image.open("3.png") #importando a imagem de fundo
+photo = ImageTk.PhotoImage(image) #definindo a imagem
+label = tk.Label(root, image=photo, bd=0) #definindo a imagem na janela principal
+label.image = photo #inserindo a imagem de fundo
+label.grid(row=2, column=0)  # posicionamento da imagem 
 
 # ícone
 root.iconbitmap("icone.ico")
@@ -34,25 +35,27 @@ texto = tk.Label(root,
                  font=myFont1, bg='black', fg='#FFFFDE')  # fg cor da fonte
 texto.place(x=20, y=30)
 
-
+#função do webbrowser para abrir qualquer link no navegador
 def OpenUrl():
     webbrowser.open_new(url)
 
 
-# site do botão 1
+# site do botão aleatório
 url = "https://drive.google.com/file/d/1k1NZAi-jxMmFqyIGfHuDlOPv4vU-hmTc/view?usp=sharing"
 
-# fonte do botão
+# fonte do botão aleatório
 myFont = tkinter.font.Font(family='Candara', weight='bold', size=10)
 
-# botão do cabeçalho
+# criação do botão aleatório
 button = tk.Button(root, text="QUERO LER UM LIVRO\nALEATÓRIO!", command=lambda aurl=url: OpenUrl(), bg='indigo',
                    borderwidth=5, font=myFont, relief="ridge", fg='#ffffff', cursor="hand2")
+#localização do botão aleatório(o place permite ser alocado no local especifícado por x e y)
 button.place(x=393, y=30)
 
 
-# combobox 01
+# COMBOBOX 01 - SUSPENSE
 
+#função do combobox 01
 def livro_selecionado(event):
     dicio = {
         ' Billy Summers - EN ': 'https://www.google.com.br/books/edition/Billy_Summers/L3oXEAAAQBAJ?hl=pt-BR&gbpv=1&dq=billy+summers&printsec=frontcover',
@@ -66,12 +69,12 @@ def livro_selecionado(event):
 
     showinfo(title='Boa leitura!', message=msg)
 
-
+#estilo do combobox 01
 combo1 = tk.StringVar()
 fontStyle = ("Candara", 11, "italic")
 escolha1 = ttk.Combobox(root, width=23, textvariable=combo1, state='readonly', font=fontStyle)
 
-# Adding combobox drop down list
+# adicionado a lista no combobox 01
 escolha1['values'] = (' Billy Summers - EN ',
                       ' Outsider',)
 
@@ -80,12 +83,15 @@ escolha1.current()
 
 escolha1.bind('<<ComboboxSelected>>', livro_selecionado)
 
+#título do combobox 01
 texto1 = tk.Label(root, text='                                      Suspense', font=fontStyle, bg='black', fg='#9B10B0',
                   width=23, height=1)
 texto1.place(x=15, y=150)
 
 
-# combobox 02
+# COMBOBOX 02 -FICÇÃO CIENTÍFICA
+
+#função do combobox 02
 def livro_selecionado2(event):
     dicio2 = {
         ' Flores para Algernon ': 'https://www.google.com.br/books/edition/Flores_Para_Algernon/KBdiDwAAQBAJ?hl=pt-BR&gbpv=1&dq=flores+para+algernon&printsec=frontcover',
@@ -104,7 +110,7 @@ combo2 = tk.StringVar()
 fontStyle2 = ("Candara", 11, "italic")
 escolha2 = ttk.Combobox(root, width=23, textvariable=combo1, state='readonly', font=fontStyle2)
 
-# adicionando os livros a lista
+# adicionando os livros no combobox 02
 escolha2['values'] = (' Flores para Algernon ',
                       ' Frankstein ',)
 
@@ -127,7 +133,7 @@ buttonfeed = tk.Button(root, text="feedback", command=lambda aurl=urlfeed: FeedU
                        font=myFontfeed, relief="sunken", fg='#ffffff', cursor="hand2")
 buttonfeed.place(x=1, y=661)
 
-# título do combobox2
+# título do combobox 02
 texto2 = tk.Label(root, text='                          Ficção Científica', font=fontStyle2, bg='black', fg='#9B10B0',
                   width=23, height=1)
 texto2.place(x=257, y=150)
@@ -136,5 +142,5 @@ texto2.place(x=257, y=150)
 assinatura = tk.Label(root, text='evilly', font=fontStyle2, bg='black', fg='#6C0B7A')
 assinatura.place(x=518, y=670)
 
-# mantém aberta a janela
+# mantém a janela aberta
 root.mainloop()
